5f4c778d2edfdd7a364e1929ab6a6b670f842ea1
THÔNG MINH <huynhminhthong1912@gmail.com>
2021-08-06 18:54:05 +0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DemoComponent } from './demo/demo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DemoComponent //các component trong module này (mỗi component sinh ra phải ở trong 1 module)
   ],
   imports: [
-    BrowserModule
+    BrowserModule //nơi chèn các module khác vào
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [], //nơi chèn các service vào
+  bootstrap: [AppComponent] //Các component 
 })
-export class AppModule { }
+export class AppModule { } //Module gốc ứng dụng 
---
+++ src/app/demo/demo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-demo', //tên thẻ
+  templateUrl: './demo.component.html', // đường dẫn đến file chứa giao diện
+  styleUrls: ['./demo.component.scss'] // đường dẫn đến file css
+})
+export class DemoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
